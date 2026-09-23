0d696027e5dfc698feb132e49478d01b348d82f7
saygilicihan4-lgtm <saygilicihan4@gmail.com>
2026-09-24 02:20:55 +0300

    Wire CRC ready output in standalone RTL regression

--- rtl_tb/tb_qart_qhap_crc_stream.sv
+++ rtl_tb/tb_qart_qhap_crc_stream.sv
@@ -1,5 +1,5 @@
 module tb_qart_qhap_crc_stream;
- logic clk=0,rst_n=0,start=0,beat_valid=0;logic[31:0]tdata;logic[2:0]beat_index;logic[31:0]crc;logic[31:0]mem[0:7];always #1 clk=~clk;
+ logic clk=0,rst_n=0,start=0,beat_valid=0,ready;logic[31:0]tdata;logic[2:0]beat_index;logic[31:0]crc;logic[31:0]mem[0:7];always #1 clk=~clk;
  qart_qhap_crc_stream dut(.*);
  initial begin
   $readmemh("tests/qhap_e2e_words.hex",mem);
