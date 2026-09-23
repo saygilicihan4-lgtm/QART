d24e43a818ea6775ed9833a911a60e484dc653dd
saygilicihan4-lgtm <saygilicihan4@gmail.com>
2026-09-24 01:09:16 +0300

    Start autonomous QHAP E2E sequence at reset value zero

--- tests/generate_qhap_e2e.py
+++ tests/generate_qhap_e2e.py
@@ -1,5 +1,5 @@
 from hardware.qhap_frame import pack_frame
-f=pack_frame(kind=1,flags=0,seq=7,channel=3,action=1,amp=0x4000,duration=100,p0=0x11223344,p1=0x55667788)
+f=pack_frame(kind=1,flags=0,seq=0,channel=3,action=1,amp=0x4000,duration=100,p0=0x11223344,p1=0x55667788)
 assert len(f)==32
 with open("tests/qhap_e2e_words.hex","w") as out:
     for i in range(0,32,4):
